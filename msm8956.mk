f7a903378c1902c7f778c1fe2c6fc84511a3051c
TheScarastic <warabhishek@gmail.com>
2017-08-29 00:20:27 +0530

    msm8956: User hidl multihal

--- msm8956.mk
+++ msm8956.mk
@@ -271,8 +271,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl \
-    sensors.msm8952
+    android.hardware.sensors@1.0-impl
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \

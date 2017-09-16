e537ae0724be47988a3d9ac1d7a1a51a835d4ee9
TheScarastic <warabhishek@gmail.com>
2017-09-16 18:42:00 +0530

    msm8956: Build camera.device@1.0-impl

--- msm8956.mk
+++ msm8956.mk
@@ -114,6 +114,7 @@ PRODUCT_PACKAGES += \
     libqomx_core \
     libmm-qcamera \
     Snap \
+    camera.device@1.0-impl \
     camera.device@3.2-impl \
     android.hardware.camera.provider@2.4-impl
 
